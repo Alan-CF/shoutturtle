d68d7051a04bf35a5280191b760c777b5446b61c
Alan Canales <156135214+Alan-CF@users.noreply.github.com>
2024-02-08 19:07:15 -0600

    Add files via upload

---
+++ JS/sketch.js
@@ -0,0 +1,40 @@
+
+var mic;
+var vol;
+let imgs = [];
+
+function preload() {
+    imgs = [loadImage("JS/images/normal.png"), loadImage("JS/images/scared.jpg")];
+
+}
+function setup() {
+    createCanvas(windowWidth,windowHeight);
+    
+    mic = new p5.AudioIn();
+    mic.start();
+    getAudioContext().resume();
+   
+}
+
+function draw() {
+    background(255,255,255);
+    vol = mic.getLevel();
+    if (vol < 0.02) {
+        image(imgs[0], (windowWidth/2 ) - 100, (windowHeight/2 ) - 100, 200, 200);
+    } else {
+        image(imgs[1], (windowWidth/2) - 100, (windowHeight/2) - 100, 200, 200);
+    }
+    
+    text("Alan C", windowWidth - 50, windowHeight -10);
+    text(vol * 100, 30, 30);
+    text(" Esta pagina fue hecha para que Vicky\npueda asustar a las tortugas de regreso.", (windowWidth/2) - 110, 100);
+}
+
+function touchStarted() {
+    getAudioContext().resume();
+  }
+
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
+    
